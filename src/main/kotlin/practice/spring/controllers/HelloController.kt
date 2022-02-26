9f23e5ef03c889303a265787cc241f43b0e8c845
kmebin <blowhuiee@gmail.com>
2022-02-26 14:27:00 +0900

    [ADD] View 환경설정

---
+++ src/main/kotlin/practice/spring/controllers/HelloController.kt
@@ -0,0 +1,16 @@
+package practice.spring.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+
+    @GetMapping("hello")
+    fun hello(model: Model): String {
+        model.addAttribute("data", "hello!")
+
+        return "hello"
+    }
+}
