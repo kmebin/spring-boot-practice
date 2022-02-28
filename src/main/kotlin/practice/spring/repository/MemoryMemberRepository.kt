5daf7c73b2b051ce31ceabe12a5c9fcb30796bb9
kmebin <blowhuiee@gmail.com>
2022-03-01 01:18:41 +0900

    [UPDATE] Scope 함수 적용

--- src/main/kotlin/practice/spring/repository/MemoryMemberRepository.kt
+++ src/main/kotlin/practice/spring/repository/MemoryMemberRepository.kt
@@ -2,33 +2,22 @@ package practice.spring.repository
 
 import practice.spring.domain.Member
 
-class MemoryMemberRepository : MemberRepository {
+class MemoryMemberRepository(
+    private val store: MutableMap<Long, Member> = HashMap(),
+    private var sequence: Long = 0L
+) : MemberRepository {
 
-    override fun add(member: Member): Member {
-        member.id = ++sequence
-        store[member.id] = member
+    override fun save(member: Member) = member
+        .apply {
+            id = ++sequence
+            store[id] = member
+        }
 
-        return member
-    }
+    override fun findById(id: Long) = store[id]
 
-    override fun findById(id: Long): Member? {
-        return store[id]
-    }
+    override fun findByName(name: String) = store.values.find { it.name == name }
 
-    override fun findByName(name: String): Member? {
-        return store.values.find { it.name == name }
-    }
+    override fun findAll() = ArrayList(store.values)
 
-    override fun findAll(): List<Member> {
-        return ArrayList(store.values)
-    }
-
-    fun clear() {
-        store.clear()
-    }
-
-    companion object {
-        private val store: MutableMap<Long, Member> = HashMap()
-        private var sequence = 0L
-    }
+    fun clear() = store.clear()
 }
