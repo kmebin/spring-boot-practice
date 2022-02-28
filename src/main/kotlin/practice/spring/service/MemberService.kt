7389816c1d6beb475dc3cb03f43590bf477e4cae
kmebin <blowhuiee@gmail.com>
2022-03-01 01:51:51 +0900

    [UPDATE] 특정 회원 조회 시 예외 처리 추가

--- src/main/kotlin/practice/spring/service/MemberService.kt
+++ src/main/kotlin/practice/spring/service/MemberService.kt
@@ -14,11 +14,10 @@ class MemberService(
         }
 
     private fun checkDuplicateMember(member: Member) = memberRepository.findByName(member.name)
-        ?.run {
-            throw IllegalStateException("이미 존재하는 회원입니다.")
-        }
+        ?.run { throw IllegalStateException("이미 존재하는 회원입니다.") }
 
     fun findAllMembers() = memberRepository.findAll()
 
     fun findMember(memberId: Long) = memberRepository.findById(memberId)
+        ?.run { throw IllegalStateException("존재하지 않는 회원입니다.") }
 }
