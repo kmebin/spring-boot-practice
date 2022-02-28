9002bdc3e504ae09c0e030ff3b32922b056f225b
kmebin <blowhuiee@gmail.com>
2022-02-28 18:26:03 +0900

    [ADD] Member Repository 구현체

---
+++ src/main/kotlin/practice/spring/repository/MemoryMemberRepository.kt
@@ -0,0 +1,30 @@
+package practice.spring.repository
+
+import practice.spring.domain.Member
+
+class MemoryMemberRepository : MemberRepository {
+
+    override fun add(member: Member): Member {
+        member.id = ++sequence
+        store[member.id] = member
+
+        return member
+    }
+
+    override fun findById(id: Long): Member? {
+        return store[id]
+    }
+
+    override fun findByName(name: String): Member? {
+        return store.values.find { it.name == name }
+    }
+
+    override fun findAll(): List<Member> {
+        return ArrayList(store.values)
+    }
+
+    companion object {
+        private val store: MutableMap<Long, Member> = HashMap()
+        private var sequence = 0L
+    }
+}
