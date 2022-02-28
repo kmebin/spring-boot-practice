450d72d0332b68312cc8fdbfbe4b3a14932a9688
kmebin <blowhuiee@gmail.com>
2022-03-01 01:09:51 +0900

    [UPDATE] 함수명 save로 수정

--- src/main/kotlin/practice/spring/repository/MemberRepository.kt
+++ src/main/kotlin/practice/spring/repository/MemberRepository.kt
@@ -3,7 +3,7 @@ package practice.spring.repository
 import practice.spring.domain.Member
 
 interface MemberRepository {
-    fun add(member: Member): Member
+    fun save(member: Member): Member
     fun findById(id: Long): Member?
     fun findByName(name: String): Member?
     fun findAll(): List<Member>
--- src/test/kotlin/practice/spring/repository/MemoryMemberRepositoryTest.kt
+++ src/test/kotlin/practice/spring/repository/MemoryMemberRepositoryTest.kt
@@ -10,15 +10,13 @@ class MemoryMemberRepositoryTest {
     private val repository = MemoryMemberRepository()
 
     @AfterEach
-    fun clear() {
-        repository.clear()
-    }
+    fun clear() = repository.clear()
 
     @Test
-    fun add() {
+    fun save() {
         val member = Member(name = "spring")
 
-        repository.add(member)
+        repository.save(member)
 
         val result = repository.findById(member.id)
 
@@ -30,8 +28,8 @@ class MemoryMemberRepositoryTest {
         val member1 = Member(name = "spring1")
         val member2 = Member(name = "spring2")
 
-        repository.add(member1)
-        repository.add(member2)
+        repository.save(member1)
+        repository.save(member2)
 
         val result = repository.findByName("spring1")
 
@@ -43,8 +41,8 @@ class MemoryMemberRepositoryTest {
         val member1 = Member(name = "spring1")
         val member2 = Member(name = "spring2")
 
-        repository.add(member1)
-        repository.add(member2)
+        repository.save(member1)
+        repository.save(member2)
 
         val result = repository.findAll()
 
