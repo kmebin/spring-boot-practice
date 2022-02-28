c022576ad745577b37ef4112667b3a7d6959424d
kmebin <blowhuiee@gmail.com>
2022-03-01 01:19:20 +0900

    [ADD] Member Service

---
+++ src/main/kotlin/practice/spring/service/MemberService.kt
@@ -0,0 +1,24 @@
+package practice.spring.service
+
+import practice.spring.domain.Member
+import practice.spring.repository.MemberRepository
+
+class MemberService(
+    private val memberRepository: MemberRepository
+) {
+    fun signup(member: Member) = member
+        .run {
+            checkDuplicateMember(member)
+            memberRepository.save(member)
+            id
+        }
+
+    private fun checkDuplicateMember(member: Member) = memberRepository.findByName(member.name)
+        ?.run {
+            throw IllegalStateException("이미 존재하는 회원입니다.")
+        }
+
+    fun findAllMembers() = memberRepository.findAll()
+
+    fun findMember(memberId: Long) = memberRepository.findById(memberId)
+}
